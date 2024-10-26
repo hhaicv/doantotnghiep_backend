96b3d99f5272a729df728f57c20a7a04257c4c33
hhai03 <mahoanghaicv@gmail.com>
2024-10-26 17:01:03 +0700

    hhai_updatedatabase

--- database/migrations/2024_10_26_095827_create_promotion_users_table.php
+++ database/migrations/2024_10_26_095827_create_promotion_users_table.php
@@ -13,13 +13,8 @@ return new class extends Migration
     {
         Schema::create('promotion_users', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Thêm cột user_id và tạo khóa ngoại
-            $table->foreignId('promotion_id')->constrained('promotions')->onDelete('cascade'); // Thêm cột promotion_id và tạo khóa ngoại
-            $table->timestamp('used_at')->nullable();
-            $table->softDeletes();
             $table->timestamps();
         });
-        
     }
 
     /**
--- database/migrations/2024_10_26_095914_create_payment_methods_table.php
+++ database/migrations/2024_10_26_095914_create_payment_methods_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('payment_methods', function (Blueprint $table) {
             $table->id();
-            $table->enum('method_name', ['MoMo', 'VNPAY']);
             $table->timestamps();
         });
     }
