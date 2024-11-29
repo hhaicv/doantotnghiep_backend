133356d36b86ebfce8d5ce2fed1bff6734fa1ae4
phungnhung <emlahnhung@gmail.com>
2024-11-29 14:18:54 +0700

    nhung_fixRoles

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -13,35 +13,49 @@
     </div>
    
     @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Thành công',
+                    text: "{{ session('success') }}"
+                });
+            });
+        </script>
     @endif
-    @if (session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
+    @if (session('failes'))
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Thất bại',
+                    text: "{{ session('failes') }}"
+                });
+            });
+        </script>
     @endif
+
     <div class="card">
         <form action="{{ route('admin.roles.update', $model) }}" method="POST" class="row g-3 p-5">
             @csrf
             @method('PUT')
             <div class="col-md-12">
                 <label for="fullnameInput" class="form-label">Tên quyền</label>
-                <input type="text" class="form-control" id="name_role" name="name_role" value="{{ $model->name_role }}">
+                <input type="text" class="form-control" id="name_role" name="name_role"
+                value="{{ old('name_role', $model->name_role) }}">
+                @error('name_role')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
+
             <div class="col-md-12">
                 <label for="exampleFormControlTextarea5" class="form-label">Mô tả</label>
-                <textarea class="form-control" id="exampleFormControlTextarea5" name="description" rows="2">{{ $model->description }}</textarea>
+                <textarea class="form-control" id="exampleFormControlTextarea5" name="description" rows="2">{{ old('description', $model->description) }}</textarea>
+                @error('description')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </div>
-            {{-- <div class="form-check form-switch form-switch-primary mt-3">
-                <input type="hidden" name="is_active" value="0">
-                <input class="form-check-input" type="checkbox" role="switch" id="CheckRoles" name="is_active"
-                    {{ $model->is_active ? 'checked' : '' }} value="1" onchange="toggleLabel()">
-                <label class="form-check-label" id="statusLabel" for="CheckRoles">
-                    {{ $model->is_active ? 'On' : 'Off' }}
-                </label>
-            </div> --}}
+            
             <div class="col-12">
                 <div class="text-end">
                     <button type="submit" class="btn btn-primary">Update</button>
@@ -50,17 +64,4 @@
             </div>
         </form>
     </div>
-    {{-- <script>
-        // JavaScript để cập nhật nhãn khi bật/tắt checkbox
-        function toggleLabel() {
-            var checkbox = document.getElementById('CheckRoles');
-            var label = document.getElementById('statusLabel');
-
-            if (checkbox.checked) {
-                label.textContent = 'On';
-            } else {
-                label.textContent = 'Off';
-            }
-        }
-    </script> --}}
 @endsection
